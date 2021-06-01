0de9888490aa46ebf8f46eb46194290759f0aa25
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-01 11:41:38 +0530

    commit UC12

--- js/form.js
+++ js/form.js
@@ -1,6 +1,48 @@
-const salary = document.querySelector("#salary");
-const output = document.querySelector(".salary-output");
-output.textContent = salary.value;
-salary.addEventListener("input", function () {
+window.addEventListener("DOMContentLoaded", (event) => {
+  const name = document.querySelector("#name");
+  const textError = document.querySelector(".text-error");
+  name.addEventListener("input", function () {
+    if (name.value.length == 0) {
+      textError.textContent = "";
+      return;
+    }
+    try {
+      new EmployeePayroll().name = name.value;
+      textError.textContent = "";
+    } catch (e) {
+      textError.textContent = e;
+    }
+  });
+
+  const salary = document.querySelector("#salary");
+  const output = document.querySelector(".salary-output");
   output.textContent = salary.value;
+  salary.addEventListener("input", function () {
+    output.textContent = salary.value;
+  });
 });
+
+document.getElementById("submit").onclick = function () {
+  let employee = new EmployeePayroll();
+  employee.name = document.getElementById("name").value;
+  employee.picture = document.querySelector(
+    "input[name = profile]:checked"
+  ).value;
+  employee.gender = document.querySelector(
+    "input[name = gender]:checked"
+  ).value;
+  employee.department = document.querySelector(
+    "input[name = department]:checked"
+  ).value;
+  employee.salary = document.getElementById("salary").value;
+  employee.notes = document.getElementById("notes").value;
+  employee.startDate = new Date(
+    parseInt(document.getElementById("year").value),
+    parseInt(document.getElementById("month").value),
+    parseInt(document.getElementById("day").value)
+  );
+};
+
+document.getElementById("reset").onclick = function () {
+  document.getElementById("emp-form").reset();
+};
