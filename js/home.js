0bf5280db7a4a72427ed1845d8f794b8691f1f52
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-07 12:54:29 +0530

    commit UC20

--- js/home.js
+++ js/home.js
@@ -1,28 +1,65 @@
+const createEmployeePayrollJSON = () => {
+  let empPayrollListLocal = [
+    {
+      _name: "Shruti Verma",
+      _gender: "Female",
+      _department: ["Engineering", "Finance"],
+      _salary: "300000",
+      _startDate: "8 June 2020",
+      _note: "",
+      _id: new Date().getTime(),
+      _profilePic: "../Assets/profile-images/Ellipse -3.png",
+    },
+    {
+      _profilePic: "../Assets/profile-images/Ellipse -1.png",
+      _name: "Arpit Verma",
+      _gender: "Male",
+      _department: ["HR", "Sale"],
+      _salary: "200000",
+      _startDate: "15 April 2020",
+      _note: "",
+      _id: new Date().getTime(),
+    },
+  ];
+  return empPayrollListLocal;
+};
+
 window.addEventListener("DOMContentLoaded", (event) => {
   createInnerHtml();
 });
 
-const createInnerHtml = () => {
-  const headerHtml =
-    "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
-  const innerHtml = `${headerHtml}
-    <tr>
-    <td>
-        <img class="profile" alt="" src="../assets/profile-images/Ellipse -4.png">
-    </td>
-    <td>Arpit Verma</td>
-    <td>Male</td>
-    <td>
-        <div class="dept-label">HR</div>
-        <div class="dept-label">Engineering</div>
-    </td>
-    <td>600000</td>
-    <td>19 July 2019</td>
-    <td>
-        <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-        <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
-    </td>
- </tr>
-    `;
+function createInnerHtml() {
+  const headerHtml = `<tr><th></th><th>Name</th><th>Gender</th><th>Department</th>
+      <th>Salary</th><th>start Date</th><th>Actions</th></tr>`;
+  let innerHtml = `${headerHtml}`;
+  let empPayrollList = createEmployeePayrollJSON();
+  for (const empPayrollData of empPayrollList) {
+    innerHtml = `${innerHtml}
+      <tr>
+      <td><img src="${
+        empPayrollData._profilePic
+      }" class="profile" width="30px" alt=""></td>
+      <td>${empPayrollData._name}</td>
+      <td>${empPayrollData._gender}</td>
+      <td>${getDeptHtml(empPayrollData._department)}</td>
+      <td>${empPayrollData._salary}</td>
+      <td>${empPayrollData._startDate}</td>
+      <td>
+          <img id="1" name="${
+            empPayrollData._id
+          }" onclick="remove(this)" alt="delete" width="30px" src="../Assets/icons/delete-black-18dp.svg">
+          <img id="1" name="${
+            empPayrollData._id
+          }" onclick="update(this)" alt="edit" width="30px" src="../Assets/icons/create-black-18dp.svg  ">
+      </td>
+  </tr>`;
+  }
   document.querySelector("#display").innerHTML = innerHtml;
-};
+}
+function getDeptHtml(deptList) {
+  let deptHtml = "";
+  for (const dept of deptList) {
+    deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
+  }
+  return deptHtml;
+}
